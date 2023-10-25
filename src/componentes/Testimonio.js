01a877106b1ad45484609dedee7fff73b3efc01a
JoseGeek78 <126101249+JoseGeek78@users.noreply.github.com>
2023-10-25 17:51:04 +0200

    Update

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -3,7 +3,7 @@ import React from "react";
 function Testimonio() {
   return (
     <div className='contenedor-testimonio'>
-      <img className='imagen-testimonio' />
+      <img className='imagen-testimonio' src={require('../imagenes/testimonio-dipper.jpg')} alt='foto de dipper'/>
     </div>
   );
 }
