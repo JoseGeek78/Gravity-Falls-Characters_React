ed08ba709012c26dfb4b39b1c36d3f6037ede435
JoseGeek78 <126101249+JoseGeek78@users.noreply.github.com>
2023-11-01 19:55:34 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Testimonio from './componentes/Testimonio.js'
 function App() {
   return (
     <div className="App">
+       <div className='contenedor-principal'>
+      <h1>Personajes principales de 'Gravity Falls':</h1>
       <Testimonio />
+      </div>
     </div>
   );
 }
