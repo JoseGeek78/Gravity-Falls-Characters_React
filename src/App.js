30bfdea179718013d99191688d91840c2fa8c337
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-03 18:05:49 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,17 @@ function App() {
         nombre='Mason "Dipper" Pines'
         cargo='(Aficionado al misterio)'
         testimonio='Durante las vacaciones, trabajaba en la Cabaña del Misterio junto a su hermana Mabel, Soos, Stan y Wendy. Un día, descubre accidentalmente un libro en el que aparecen advertencias e información sobre los sucesos del pueblo (el Diario Número "3"). Es atento e inteligente, y para no fallar, organiza listas a menudo, con la intención de que todo salga perfecto. Dipper está enamorado de Wendy y, en más de una ocasión, ha intentando confesarle su amor.'
-        imagen=''/>
+        imagen='dipper'/>
+         <Testimonio 
+        nombre='Mabel Pines'
+        cargo='(Aficionada a la purpurina y a Bamboleos)'
+        testimonio='Mabel es una chica optimista y alegre, que siempre ve el lado positivo de las cosas. Durante la primera temporada mantuvo una obsesión por tener un romance de verano, donde demostraba una alta seguridad en sí misma. Su actitud caótica contrasta la personalidad introvertida y cautiva de su hermano, Dipper. Cuando se siente deprimida o simplemente triste se sube el cuello del suéter a nivel de la cara y dice estar en "Sueterlandia".'
+        imagen='mabel'/>
+         <Testimonio 
+        nombre='Stan Pines'
+        cargo='(Aficionado al dinero)'
+        testimonio='A pesar de que envía a los gemelos a hacer recados impredecibles y vergonzosos, siempre posee los mejores intereses en su corazón y los ama de forma incondicional. Mientras tanto, cuando no trata de ganar dinero rápidamente por medio de sus clientes incoherentes, el Tío Stan guarda su secreto personal, que podría ser la clave para descubrir el misterio de Gravity Falls.'
+        imagen='stan'/>
       </div>
     </div>
   );
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -6,10 +6,10 @@ function Testimonio(props) {
     <div className='contenedor-testimonio'>
       <img 
       className='imagen-testimonio' 
-      src={require('../imagenes/testimonio-dipper.jpg')} 
+      src={require(`../imagenes/testimonio-${props.imagen}.jpg`)} 
       alt='foto de dipper'/>
     <div className="contenedor-texto-testimonio">
-           <p className="nombre-testimonio">{props.nombre}</p>
+           <p className="nombre-testimonio"><strong>{props.nombre}</strong></p>
            <p className="cargo-testimonio">{props.cargo}</p>
            <p className="texto-testimonio">{props.testimonio}</p>
       </div>
